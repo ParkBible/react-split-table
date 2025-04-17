a10dc33047edaa2e8e17a7cc3d5e3f8de0ee5042
ParkBible <87624953+ParkBible@users.noreply.github.com>
2025-04-17 20:56:54 +0900

    feat: 테이블 분할 기능 구현 (#6)
    * feat: 테이블 분할 기능 구현중
    
    * feat: 줄바꿈 감지 기능
    
    * feat: 테이블 분할 기능 추가
    
    * style: 프리티어를 통해 코드 포맷팅
    
    * Feature/ci (#7)
    
    * chore: vitest 설치 및 CI 세팅
    
    * chore: 테스트 실행 스크립트 수정, 테스트용 테스트 코드 추가
    
    * chore: 라이선스 추가 (#8)
    
    * chore: debounce 적용 (#10)
    
    * chore: 사용자가 prop을 통해 TableWrapper의 class name을 직접 설정할 수 있도록 수정

---
+++ .prettierrc.js
@@ -0,0 +1,9 @@
+export default {
+    singleQuote: false,
+    semi: true,
+    bracketSpacing: false,
+    arrowParens: "avoid",
+    jsxBracketSameLine: false,
+    tabWidth: 4,
+    printWidth: 100,
+};
---
+++ src/components/TableWrapper.tsx
@@ -0,0 +1,113 @@
+import {useEffect, useRef, useState} from "react";
+import "../styles/table.css";
+import {TableProp, TableWrapperProp} from "../constants/tableInterfaces";
+import Table from "./Table";
+import {debounce} from "../util/debounce";
+
+export const TableWrapper = (props: TableWrapperProp) => {
+    const {columns, rows, className} = props;
+
+    const [wrapperWidth, setWrapperWidth] = useState<number>(0);
+    const [tablesInfo, setTablesInfo] = useState<TableProp[]>([]);
+    const tableWrapperRef = useRef<HTMLDivElement>(null);
+
+    useEffect(() => {
+        setTablesInfo([
+            {
+                columns: columns.sort((a, b) => a.order - b.order),
+                rows: rows,
+                tableNum: 0,
+            },
+        ]);
+    }, []);
+
+    useEffect(() => {
+        if (tablesInfo.length === 0) return;
+
+        const resizeHandler = debounce((entries: ResizeObserverEntry[]) => {
+            for (let entry of entries) {
+                const width = entry.contentRect.width;
+                setWrapperWidth(width);
+            }
+        }, 100);
+
+		const observer = new ResizeObserver(resizeHandler);
+        observer.observe(tableWrapperRef.current!);
+
+        return () => {
+            observer.disconnect();
+        };
+    }, [tablesInfo]);
+
+    const splitTable = (tableNum: number) => {
+        setTablesInfo(prev => {
+            const targetTableIndex = prev.findIndex(t => t.tableNum === tableNum);
+            if (targetTableIndex === -1) return prev;
+
+            const targetTable = prev[targetTableIndex];
+            const {columns, rows} = targetTable;
+
+            const isLastTable = tableNum === prev.length - 1;
+            const columnCount = columns.length;
+
+            if (columnCount < 2) return prev;
+            const splitIndex = columnCount - 1;
+
+            const firstHalfColumns = columns.slice(0, splitIndex);
+            const secondHalfColumns = columns.slice(splitIndex);
+
+            const firstHalf = {
+                columns: firstHalfColumns,
+                rows,
+                tableNum: tableNum,
+            };
+
+            const secondHalf = {
+                columns: secondHalfColumns,
+                rows,
+                tableNum: tablesInfo.length,
+            };
+
+            const newTablesInfo = JSON.parse(JSON.stringify(prev)) as TableProp[];
+
+            // 마지막 테이블이 아니라면, 그 다음 테이블과 합친다.
+            // 마지막 테이블이라면 테이블 하나를 추가한다.
+            if (!isLastTable) {
+                const nextTable = prev.find(t => t.tableNum === tableNum + 1);
+
+                if (nextTable) {
+                    newTablesInfo[targetTableIndex] = {
+                        ...firstHalf,
+                        columns: firstHalfColumns,
+                    };
+
+                    const nextTableColumns = nextTable.columns.concat(secondHalfColumns);
+
+                    newTablesInfo[targetTableIndex + 1] = {
+                        ...nextTable,
+                        columns: nextTableColumns.sort((a, b) => a.order - b.order),
+                    };
+                }
+            } else {
+                newTablesInfo.splice(targetTableIndex, 1, firstHalf, secondHalf);
+            }
+
+            return newTablesInfo;
+        });
+    };
+
+    return (
+        <div className={className} ref={tableWrapperRef}>
+            {tablesInfo.map(info => (
+                <Table
+                    key={info.tableNum}
+                    info={info}
+                    wrapperWidth={wrapperWidth}
+                    onCellOverflowing={splitTable}
+                />
+            ))}
+        </div>
+    );
+};
+
+export default TableWrapper;
