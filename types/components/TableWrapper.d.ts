200c9e39c97d2bfeba93fe44db43ace5b88433d7
ParkBible <87624953+ParkBible@users.noreply.github.com>
2025-04-19 02:56:43 +0900

    Feature/publish (#23)
    * chore: npm publish 깃헙 액션 추가
    
    - peerDependency 설정
    
    * fix: package-lock.json 업데이트
    
    * fix: react-dom 삭제된거 롤백
    
    * fix: 빌드 명령어 오타 수정
    
    * fix: 오타 수정
    
    * fix: react-dom 롤백
    
    * chore: package-lock.json 동기화
    
    * chore: react, react-dom 의존성 추가
    
    * chore: 배포시 필요한 파일만 올라가도록 세팅
    
    * chore: 버전 변경
    
    * chore: 라이브러리 모드 설정

---
+++ types/components/TableWrapper.d.ts
@@ -0,0 +1,4 @@
+import "../styles/table.css";
+import { TableWrapperProp } from "../constants/tableInterfaces";
+export declare const TableWrapper: (props: TableWrapperProp) => import("react/jsx-runtime").JSX.Element;
+export default TableWrapper;
